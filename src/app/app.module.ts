9b11623007920fcc97abf12972804629516b4406
Paulo Alexandre <paulofore@gmail.com>
2018-07-24 20:35:56 -0300

    Ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 
 import { appRoutes } from './rotas';
+import { FormsModule }   from '@angular/forms';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { appRoutes } from './rotas';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
