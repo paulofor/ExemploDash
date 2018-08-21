47e9384c23f32772df5a97364f85e4d0eb6765c0
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-08-21 15:56:39 -0300

    exemplo

--- src/app/layout-routing/layout-routing.module.ts
+++ src/app/layout-routing/layout-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from '../layout/layout.component';
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
 
 
 const routes: Routes = [
   {
-      path: '', component: LayoutComponent
+      path: '', component: DashboardComponent
   } 
 ];
 
